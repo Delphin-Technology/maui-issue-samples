95ed93973d045cd94599640e6c0e9e19fcd43a55
agawrischenko <andrej.Gawrischenko@delphin.de>
2024-01-04 10:40:08 +0100

    reproduces entry field crash

---
+++ DemoViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maui_issue_samples
+{
+    class DemoViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public String Text
+        {
+            get => _text;
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private String _text = String.Empty;
+
+        public DemoViewModel()
+        {
+            Text = "Bla";
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+    }
+
+}
