268a4164566c3a2b3dc48eb057f236822c1e589b
agawrischenko <andrej.Gawrischenko@delphin.de>
2024-01-04 10:27:57 +0100

    Inital commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_issue_samples
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace maui_issue_samples
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
